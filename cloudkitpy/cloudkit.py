330c34ebbb62bbdd0078a9072321986014242125
James Barrow <baza207@me.com>
2016-04-28 22:29:49 +0200

    Added basic structure of cloudkit.py.

---
+++ cloudkitpy/cloudkit.py
@@ -0,0 +1,25 @@
+#
+# cloudkit.py
+# CloudKitPy
+#
+# Created by James Barrow on 27/04/2016.
+# Copyright (c) 2013-2016 Pig on a Hill Productions. All rights reserved.
+#
+
+# !/usr/bin/env python
+
+
+class CloudKit:
+
+    def __init__(self, config):
+        """Configure CloudKitPy."""
+        pass
+
+    def get_default_container(self):
+        return None
+
+    def get_container(self, container_id):
+        return None
+
+    def get_all_containers(self):
+        return []
