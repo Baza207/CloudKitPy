4cce5d36b3fe0f0888b186766ae23baea7e8e540
James Barrow <baza207@me.com>
2016-05-01 17:28:45 +0200

    Updated CKValue to also take an optional value type parameter.

--- cloudkitpy/value.py
+++ cloudkitpy/value.py
@@ -29,13 +29,13 @@ class CKValue:
     value = None
     value_type = None
 
-    def __init__(self, value=None, json=None):
+    def __init__(self, value=None, value_type=None, json=None):
         if value is not None:
-            self.update_value(value)
+            self.update_value(value, value_type)
         elif json is not None:
             self.update_json(json)
 
-    def update_value(self, value):
+    def update_value(self, value, value_type):
         if type(value) == str:
             self.value = value
             self.value_type = 'STRING'
@@ -45,14 +45,17 @@ class CKValue:
         elif type(value) == int:
             self.value = value
             self.value_type = 'INT'
-        if type(value) == bool:
+        elif type(value) == bool:
             self.value = value
             self.value = int(value)
+            self.value_type = value_type
         elif type(value) == datetime.datetime:
             self.value = value
             self.value = int(time.mktime(value.timetuple()))
+            self.value_type = value_type
         else:
             self.value = value
+            self.value_type = value_type
 
     def update_json(self, json):
         self.value = Request.parse(json, 'value')
