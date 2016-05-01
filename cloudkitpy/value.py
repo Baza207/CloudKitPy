d7ed5008defe662664baa07b2d579587cd37e7f3
James Barrow <baza207@me.com>
2016-05-01 17:13:57 +0200

    Updated CKValue function to be a class and more adaptable.

--- cloudkitpy/value.py
+++ cloudkitpy/value.py
@@ -10,39 +10,61 @@
 
 import datetime
 import time
+from request import Request
 
 
-def CKValue(value):  # noqa
+class CKValue:
     """Describes the CloudKit web services protocol.
 
     Several common dictionaries are used by multiple requests and responses
      throughout CloudKit web services.
     """
+
     # References for Types and Dictionaries can be found at:
     # https://developer.apple.com/library/ios/documentation/DataManagement/Conceptual/CloutKitWebServicesReference/Types/Types.html
 
     # TODO: Work out how to correctly use `type` without
     # getting BAD_REQUEST errors.
 
+    value = None
     value_type = None
 
-    if type(value) == str:
-        value_type = 'STRING'
-    elif type(value) == float:
-        value_type = 'DOUBLE'
-    elif type(value) == int:
-        value_type = 'INT'
-    if type(value) == bool:
-        value = int(value)
-    elif type(value) == datetime.datetime:
-        value = int(time.mktime(value.timetuple()))
+    def __init__(self, value=None, json=None):
+        if value is not None:
+            self.update_value(value)
+        elif json is not None:
+            self.update_json(json)
 
-    if value_type is None:
-        return {
-            'value': value
-        }
-    else:
-        return {
-            'value': value,
-            'type': value_type
-        }
+    def update_value(self, value):
+        if type(value) == str:
+            self.value = value
+            self.value_type = 'STRING'
+        elif type(value) == float:
+            self.value = value
+            self.value_type = 'DOUBLE'
+        elif type(value) == int:
+            self.value = value
+            self.value_type = 'INT'
+        if type(value) == bool:
+            self.value = value
+            self.value = int(value)
+        elif type(value) == datetime.datetime:
+            self.value = value
+            self.value = int(time.mktime(value.timetuple()))
+        else:
+            self.value = value
+
+    def update_json(self, json):
+        self.value = Request.parse(json, 'value')
+        self.value_type = Request.parse(json, 'type')
+
+    def json(self):
+        if self.value_type is None:
+            return {
+                'value': self.value
+            }
+        else:
+            return {
+                'value': self.value,
+                'type': self.value_type
+            }
