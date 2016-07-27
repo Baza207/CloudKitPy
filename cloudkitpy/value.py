0e13dd9b78b70fc70ba5b5035131bb866b3a9335
James Barrow <baza207@me.com>
2016-07-27 20:22:42 +0200

    Correctly parsed date -> timestamp and vice-versa in CKValue.

--- cloudkitpy/value.py
+++ cloudkitpy/value.py
@@ -46,20 +46,25 @@ class CKValue:
             self.value = value
             self.value_type = 'NUMBER_INT64'
         elif type(value) == bool:
-            self.value = value
             self.value = int(value)
             self.value_type = value_type
         elif type(value) == datetime.datetime:
-            self.value = value
-            self.value = int(time.mktime(value.timetuple()))
-            self.value_type = value_type
+            timestamp = int(time.mktime(value.timetuple()))
+            timestamp = timestamp * 1000    # Timestamp with nanoseconds
+            self.value = timestamp
+            self.value_type = 'TIMESTAMP'
         else:
             self.value = value
             self.value_type = value_type
 
     def update_json(self, json):
-        self.value = parse(json, 'value')
         self.value_type = parse(json, 'type')
+        if self.value_type == 'TIMESTAMP':
+            timestamp = int(parse(json, 'value'))
+            timestamp = float(timestamp) / 1000    # Timestamp with nanoseconds
+            self.value = datetime.datetime.fromtimestamp(timestamp)
+        else:
+            self.value = parse(json, 'value')
 
     def json(self):
         if self.value_type is None:
